72d0db4e744b417097fdcc95bb80d3a73dc5469b
joy <suebin710@gmail.com>
2022-08-20 22:52:48 +0900

    add: iOS initial project

---
+++ iOS/TradingBot/TradingBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingBot
+//
+//  Created by exception on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
