2f3f152796b4db033f10e6d107807765e4fdcd93
sbcheon <suebin710@gmail.com>
2022-08-24 17:18:10 +0900

    update: MainActivity.kt
    - 화면 이동 로직 추가

--- Android/app/src/main/java/com/exception/tradingbot/MainActivity.kt
+++ Android/app/src/main/java/com/exception/tradingbot/MainActivity.kt
@@ -1,36 +1,62 @@
 package com.exception.tradingbot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.databinding.DataBindingUtil
 import com.chaquo.python.PyException
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import com.exception.tradingbot.R
+import com.exception.tradingbot.databinding.ActivityMainBinding
 
-
-//https://ourcodeworld.com/articles/read/1656/how-to-use-chaquopy-to-run-python-code-and-obtain-its-output-using-java-in-your-android-app
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        try {
-            if (!Python.isStarted()) {
-                Python.start(AndroidPlatform(this))
-            }
 
-            var py: Python = Python.getInstance()
-            var pyObject: PyObject = py.getModule("main")
-            pyObject.callAttr("run")
-        } catch(e: PyException) {
-            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
-            Log.e("Python", e.message.toString())
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
         }
 
 
+        mainBinding.btnStock.setOnClickListener {
+            onClickStockButton()
+        }
+
+
+        mainBinding.btnEtf.setOnClickListener {
+            onClickETFButton()
+
+        }
+
+        mainBinding.btnBuyOne.setOnClickListener {
+
+        }
 
     }
+
+    private fun onClickETFButton() {
+        val intent = Intent(this, ETFActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.animation_in, R.anim.animation_out)
+    }
+
+    private fun onClickStockButton() {
+        val intent = Intent(this, StockActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.animation_in, R.anim.animation_out)
+    }
+
 }
