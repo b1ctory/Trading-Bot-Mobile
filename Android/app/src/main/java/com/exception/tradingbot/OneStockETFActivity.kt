d12d43e240c39aaf7adb89a904afda9f22ddce62
sbcheon <suebin710@gmail.com>
2022-08-24 17:16:55 +0900

    add: OneStockETFActivity.kt

---
+++ Android/app/src/main/java/com/exception/tradingbot/OneStockETFActivity.kt
@@ -0,0 +1,10 @@
+package com.exception.tradingbot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OneStockETFActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
