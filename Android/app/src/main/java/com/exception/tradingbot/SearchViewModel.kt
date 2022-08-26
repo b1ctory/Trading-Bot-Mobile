b993307dfb52fe2a3a4251289716c8cad6bc64a5
sbcheon <suebin710@gmail.com>
2022-08-26 16:30:18 +0900

    add: SearchViewModel
    - 뷰모델로 재검색 여부 체크

---
+++ Android/app/src/main/java/com/exception/tradingbot/SearchViewModel.kt
@@ -0,0 +1,27 @@
+package com.exception.tradingbot
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel(): ViewModel() {
+
+    val isStockProgressBarStart = MutableLiveData<Boolean>()
+    fun setStockProgressBarStart(value: Boolean) {
+        isStockProgressBarStart.postValue(value)
+    }
+
+    val isStockSearchAgain = MutableLiveData<Boolean>()
+    fun setStockSearchAgain(value: Boolean) {
+        isStockSearchAgain.postValue(value)
+    }
+
+    val isETFProgressBarStart = MutableLiveData<Boolean>()
+    fun setETFProgressBarStart(value: Boolean) {
+        isStockProgressBarStart.postValue(value)
+    }
+
+    val isETFSearchAgain = MutableLiveData<Boolean>()
+    fun setETFSearchAgain(value: Boolean) {
+        isStockSearchAgain.postValue(value)
+    }
+}
