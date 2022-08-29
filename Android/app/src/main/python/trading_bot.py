59a855ed49dc77de029b2bbec00bb779b99b79e4
sbcheon <suebin710@gmail.com>
2022-08-29 17:01:15 +0900

    update: Python Trading Bot
    - Class 형식으로 수정
    - 실행 시간 3초 단축

--- Android/app/src/main/python/trading_bot.py
+++ Android/app/src/main/python/trading_bot.py
@@ -16,179 +16,146 @@ etf_tickers = pd.read_csv(etf_file)
 stock_file = join(dirname(__file__), "stock_tickers.csv")
 stock_tickers = pd.read_csv(stock_file)
 
+class Stock:
+    def __init__(self, name, df):
+        self.name = name
+        self.df = df
+        self.buy_score = -1
+        self.strategy_yield = -1
+        self.buy_and_hold_yield = -1
+        self.win_rate = -1
 
-def get_buy_position(df):
-    close = df["Adj Close"]
+    def get_indicator(self):
+        close = self.df["Adj Close"]
 
-    # 볼린저밴드
-    df["Upper"], df["Middle"], df["Lower"] = talib.BBANDS(close, timeperiod=20)
+        # 볼린저밴드
+        self.df["Upper"], self.df["Middle"], self.df["Lower"] = talib.BBANDS(close, timeperiod=20)
 
-    # 모멘텀
-    df["Momentum"] = talib.MOM(close, timeperiod=10)
-    df["Momentum_signal"] = talib.SMA(df["Momentum"], timeperiod=9)
+        # 모멘텀
+        self.df["Momentum"] = talib.MOM(close, timeperiod=10)
+        self.df["Momentum_signal"] = talib.SMA(self.df["Momentum"], timeperiod=9)
 
-    # 이평선
-    df["MA5"] = talib.SMA(close, timeperiod=5)
-    df["MA15"] = talib.SMA(close, timeperiod=20)
-    df["MA20"] = talib.SMA(close, timeperiod=20)
-    df["MA60"] = talib.SMA(close, timeperiod=60)
+        # 이평선
+        self.df["MA5"] = talib.SMA(close, timeperiod=5)
+        self.df["MA15"] = talib.SMA(close, timeperiod=20)
+        self.df["MA20"] = talib.SMA(close, timeperiod=20)
+        self.df["MA60"] = talib.SMA(close, timeperiod=60)
 
-    # RSI
-    df["RSI"] = talib.RSI(close, timeperiod=14)
-    df["RSI_signal"] = talib.SMA(df["RSI"], timeperiod=6)
+        # RSI
+        self.df["RSI"] = talib.RSI(close, timeperiod=14)
+        self.df["RSI_signal"] = talib.SMA(self.df["RSI"], timeperiod=6)
 
-    # MACD
-    df["MACD"], df["MACD_signal"], df["MACD_hist"] = talib.MACD(close)
+        # MACD
+        self.df["MACD"], self.df["MACD_signal"], self.df["MACD_hist"] = talib.MACD(close)
 
-    return df
+        if len(self.df) > 504:
+            self.df = self.df[-504:]
 
+    def simulation(self):
+        seed = 1000000
+        buy_price = 0
+        holding = False
+        fee = 0.0016
+        buy_threshold = 3
+        sell_threshold = -4
 
-def simulation(ticker, df):
-    seed = 1000000
-    buy_price = 0
-    holding = False
-    fee = 0.0016
-    buy_threshold = 3
-    sell_threshold = -4
+        temp_df = self.df.dropna()
+        temp_df = temp_df.reset_index()
+        temp_df["Score"] = 0
+        temp_dict = dict(temp_df)
 
-    temp_df = df.dropna()
-    temp_df = temp_df.reset_index()
-    temp_df["Score"] = 0
-    temp_dict = dict(temp_df)
+        for i in range(1, len(temp_dict["Close"])):
+            # 주가가 밴드 상단보다 높으면 -1
+            if temp_dict["Adj Close"][i] > temp_dict["Upper"][i]:
+                temp_dict["Score"][i] -= 2
+            # 주가가 밴드 하단보다 낮으면 +1
+            elif temp_dict["Adj Close"][i] < temp_dict["Lower"][i]:
+                temp_dict["Score"][i] += 2
 
-    for i in range(1, len(temp_dict["Close"])):
-        # 주가가 밴드 상단보다 높으면 -1
-        if temp_dict["Adj Close"][i] > temp_dict["Upper"][i]:
-            temp_dict["Score"][i] -= 2
-        # 주가가 밴드 하단보다 낮으면 +1
-        elif temp_dict["Adj Close"][i] < temp_dict["Lower"][i]:
-            temp_dict["Score"][i] += 2
+            # 모멘텀이 0보다 위일 때 +1
+            if temp_dict["Momentum"][i] > 0:
+                temp_dict["Score"][i] += 1
+            # 모멘텀이 0보다 아래일 때 -1
+            elif temp_dict["Momentum"][i] < 0:
+                temp_dict["Score"][i] -= 1
 
-        # 모멘텀이 0보다 위일 때 +1
-        if temp_dict["Momentum"][i] > 0:
-            temp_dict["Score"][i] += 1
-        # 모멘텀이 0보다 아래일 때 -1
-        elif temp_dict["Momentum"][i] < 0:
-            temp_dict["Score"][i] -= 1
+            # 모멘텀이 모멘텀 시그널을 상향돌파시 +1
+            if (temp_dict["Momentum"][i-1] < temp_dict["Momentum_signal"][i-1]) and (temp_dict["Momentum_signal"][i] < temp_dict["Momentum"][i]):
+                temp_dict["Score"][i] += 2
+            # 모멘텀이 모멘텀 시그널을 하향돌파시 -1
+            elif (temp_dict["Momentum"][i-1] > temp_dict["Momentum_signal"][i-1]) and (temp_dict["Momentum_signal"][i] > temp_dict["Momentum"][i]):
+                temp_dict["Score"][i] -= 2
 
-        # 모멘텀이 모멘텀 시그널을 상향돌파시 +1
-        if (temp_dict["Momentum"][i - 1] < temp_dict["Momentum_signal"][i - 1]) and (
-                temp_dict["Momentum_signal"][i] < temp_dict["Momentum"][i]):
-            temp_dict["Score"][i] += 2
-        # 모멘텀이 모멘텀 시그널을 하향돌파시 -1
-        elif (temp_dict["Momentum"][i - 1] > temp_dict["Momentum_signal"][i - 1]) and (
-                temp_dict["Momentum_signal"][i] > temp_dict["Momentum"][i]):
-            temp_dict["Score"][i] -= 2
+            # 단기 이평선이 장기 이평선을 상향돌파시 +1
+            if (temp_dict["MA15"][i-1] < temp_dict["MA20"][i-1]) and (temp_dict["MA20"][i] < temp_dict["MA15"][i]):
+                temp_dict["Score"][i] += 1
+            # 단기 이평선이 장기 이평선을 하향돌파시 -1
+            elif (temp_dict["MA15"][i-1] > temp_dict["MA20"][i-1]) and (temp_dict["MA20"][i] > temp_dict["MA15"][i]):
+                temp_dict["Score"][i] -= 1
 
-        # 단기 이평선이 장기 이평선을 상향돌파시 +1
-        if (temp_dict["MA15"][i - 1] < temp_dict["MA20"][i - 1]) and (
-                temp_dict["MA20"][i] < temp_dict["MA15"][i]):
-            temp_dict["Score"][i] += 1
-        # 단기 이평선이 장기 이평선을 하향돌파시 -1
-        elif (temp_dict["MA15"][i - 1] > temp_dict["MA20"][i - 1]) and (
-                temp_dict["MA20"][i] > temp_dict["MA15"][i]):
-            temp_dict["Score"][i] -= 1
+            # RSI가 70을 넘어가면 과매수 -1
+            if temp_dict["RSI"][i] >= 70:
+                temp_dict["Score"][i] -= 2
+            # RSI가 30에서 내려가면 과매도 +1
+            elif temp_dict["RSI"][i] <= 30:
+                temp_dict["Score"][i] += 2
 
-        # RSI가 70을 넘어가면 과매수 -1
-        if temp_dict["RSI"][i] >= 70:
-            temp_dict["Score"][i] -= 2
-        # RSI가 30에서 내려가면 과매도 +1
-        elif temp_dict["RSI"][i] <= 30:
-            temp_dict["Score"][i] += 2
+            # RSI가 RSI 시그널을 상향돌파시 +1
+            if (temp_dict["RSI"][i-1] < temp_dict["RSI_signal"][i-1]) and (temp_dict["RSI_signal"][i] < temp_dict["RSI"][i]):
+                temp_dict["Score"][i] += 1
+            # RSI가 RSI 시그널을 하향돌파시 -1
+            elif (temp_dict["RSI"][i-1] > temp_dict["RSI_signal"][i-1]) and (temp_dict["RSI_signal"][i] > temp_dict["RSI"][i]):
+                temp_dict["Score"][i] -= 1
 
-        # RSI가 RSI 시그널을 상향돌파시 +1
-        if (temp_dict["RSI"][i - 1] < temp_dict["RSI_signal"][i - 1]) and (
-                temp_dict["RSI_signal"][i] < temp_dict["RSI"][i]):
-            temp_dict["Score"][i] += 1
-        # RSI가 RSI 시그널을 하향돌파시 -1
-        elif (temp_dict["RSI"][i - 1] > temp_dict["RSI_signal"][i - 1]) and (
-                temp_dict["RSI_signal"][i] > temp_dict["RSI"][i]):
-            temp_dict["Score"][i] -= 1
+            # MACD가 MACD 시그널을 상향돌파시 +1
+            if (temp_dict["MACD"][i-1] < temp_dict["MACD_signal"][i-1]) and (temp_dict["MACD_signal"][i] < temp_dict["MACD"][i]):
+                temp_dict["Score"][i] += 1
+            # MACD가 MACD 시그널을 하향돌파시 -1
+            elif (temp_dict["MACD"][i-1] > temp_dict["MACD_signal"][i-1]) and (temp_dict["MACD_signal"][i] > temp_dict["MACD"][i]):
+                temp_dict["Score"][i] -= 1
 
-        # MACD가 MACD 시그널을 상향돌파시 +1
-        if (temp_dict["MACD"][i - 1] < temp_dict["MACD_signal"][i - 1]) and (
-                temp_dict["MACD_signal"][i] < temp_dict["MACD"][i]):
-            temp_dict["Score"][i] += 1
-        # MACD가 MACD 시그널을 하향돌파시 -1
-        elif (temp_dict["MACD"][i - 1] > temp_dict["MACD_signal"][i - 1]) and (
-                temp_dict["MACD_signal"][i] > temp_dict["MACD"][i]):
-            temp_dict["Score"][i] -= 1
+        self.df = pd.DataFrame.from_dict(temp_dict)
 
-    df = pd.DataFrame.from_dict(temp_dict)
+        for index, row in self.df.iterrows():
+            self.df.loc[index, "yield"] = int((seed/1000000-1)*100)
+            if row["Score"] >= buy_threshold:
+                if not holding:
+                    buy_price = row["Adj Close"]
+                    self.df.loc[index, "trade"] = "BUY"
+                    holding = True
 
-    for index, row in df.iterrows():
-        df.loc[index, "yield"] = int((seed / 1000000 - 1) * 100)
-        if row["Score"] >= buy_threshold:
-            if not holding:
-                buy_price = row["Adj Close"]
-                df.loc[index, "trade"] = "BUY"
-                holding = True
+            elif row["Score"] <= sell_threshold:
+                if holding:
+                    sell_price = row["Adj Close"]
+                    holding = False
+                    if sell_price > buy_price:
+                        self.df.loc[index, "trade"] = "SELL"
+                    else:
+                        self.df.loc[index, "trade"] = "STOP"
+                    seed = seed * (sell_price/buy_price) * (1-fee)
 
-        elif row["Score"] <= sell_threshold:
-            if holding:
+            # 5%이상 손실날 때 손절
+            elif holding and row["Adj Close"]/buy_price <= 0.9:
                 sell_price = row["Adj Close"]
                 holding = False
-                if sell_price > buy_price:
-                    df.loc[index, "trade"] = "SELL"
-                else:
-                    df.loc[index, "trade"] = "STOP"
-                seed = seed * (sell_price / buy_price) * (1 - fee)
+                self.df.loc[index, "trade"] = "STOP"
+                seed = seed * (sell_price/buy_price) * (1-fee)
 
-        # 5%이상 손실날 때 손절
-        elif holding and row["Adj Close"] / buy_price <= 0.9:
-            sell_price = row["Adj Close"]
-            holding = False
-            df.loc[index, "trade"] = "STOP"
-            seed = seed * (sell_price / buy_price) * (1 - fee)
-
-    if df.iloc[-1]["Score"] >= buy_threshold:
-        if len(df[df["trade"] == "BUY"]) != len(df[df["trade"] == "SELL"]) + len(
-                df[df["trade"] == "STOP"]):
-            win_rate = len(df[df["trade"] == "SELL"]) / (len(df[df["trade"] == "BUY"]) - 1)
+        if len(self.df[self.df["trade"]=="BUY"]) != len(self.df[self.df["trade"]=="SELL"]) + len(self.df[self.df["trade"]=="STOP"]):
+            win_rate = len(self.df[self.df["trade"]=="SELL"])/(len(self.df[self.df["trade"]=="BUY"])-1)
         else:
-            win_rate = len(df[df["trade"] == "SELL"]) / len(df[df["trade"] == "BUY"])
+            win_rate = len(self.df[self.df["trade"]=="SELL"])/len(self.df[self.df["trade"]=="BUY"])
 
-        # if win_rate <= 0.5 or round((seed / 1000000 - 1) * 100, 2) < 0:
-        #     return
+        self.df["buy_and_hold"] = round(((1000000/self.df.iloc[0]["Adj Close"]*self.df["Adj Close"])/1000000-1)*100, 2)
 
-        temp_dict = dict()
-        temp_dict["ticker"] = ticker
-        temp_dict["buy score"] = df.iloc[-1]["Score"]
-        temp_dict["전략 수익률"] = round((seed / 1000000 - 1) * 100, 2)
-        df["buynhold"] = round(((1000000 / df.iloc[0]["Adj Close"] * df["Adj Close"]) / 1000000 - 1) * 100, 2)
-        temp_dict["바이앤홀드 수익률"] = df.iloc[-1]['buynhold']
-        temp_dict["승률"] = win_rate
-
-        return temp_dict
-    else:  # 살 종목 아닐때
-        return None
+        self.buy_score = self.df.iloc[-1]["Score"]
+        self.strategy_yield = (seed/1000000-1)*100
+        self.buy_and_hold_yield = self.df.iloc[-1]['buy_and_hold']
+        self.win_rate = win_rate
 
 
-def get_indicator(df):
-    close = df["Adj Close"]
-
-    # 볼린저밴드
-    df["Upper"], df["Middle"], df["Lower"] = talib.BBANDS(close, timeperiod=20)
-
-    # 모멘텀
-    df["Momentum"] = talib.MOM(close, timeperiod=10)
-    df["Momentum_signal"] = talib.SMA(df["Momentum"], timeperiod=9)
-
-    # 이평선
-    df["MA5"] = talib.SMA(close, timeperiod=5)
-    df["MA15"] = talib.SMA(close, timeperiod=20)
-    df["MA20"] = talib.SMA(close, timeperiod=20)
-    df["MA60"] = talib.SMA(close, timeperiod=60)
-
-    # RSI
-    df["RSI"] = talib.RSI(close, timeperiod=14)
-    df["RSI_signal"] = talib.SMA(df["RSI"], timeperiod=6)
-
-    # MACD
-    df["MACD"], df["MACD_signal"], df["MACD_hist"] = talib.MACD(close)
-
-    return df
-
+def stock_to_string(self):
+    return self.name + " " + str(self.buy_score) + " " + str(self.strategy_yield) + " " + str(self.buy_and_hold_yield) + " " + str(self.win_rate)
 
 def get_sell_position(df, sell_threshold, ticker):
     df["Score"] = 0
@@ -263,67 +230,76 @@ def get_sell_position(df, sell_threshold, ticker):
     return temp_dict
 
 
+# ETF 전체 조회
 def buyETF():
-    # etf
     print("------------ETF-----------")
     etf = []
     for ticker in tqdm(etf_tickers["Symbol"]):
         try:
-            df = pdr.get_data_yahoo(ticker)
-            df = get_buy_position(df)
-            if simulation(ticker, df) is not None:
-                etf.append(simulation(ticker, df))
+            stock_df = pdr.get_data_yahoo(ticker)
+            stock = Stock(ticker, stock_df)
+            stock.get_indicator()
+            stock.simulation()
+
+            if stock.buy_score >= 3:
+                etf.append(stock_to_string(stock))
+
+            print(etf)
 
         except Exception as e:
-            print("except : " + ticker)
+            print("except")
             print(e)
             pass
 
     return etf
 
-
+# ETF 단일 종목 조회
 def buyOneETF(ticker):
-    # etf
     print("------------Buy One ETF-----------")
     try:
-        df = pdr.get_data_yahoo(ticker)
-        df = get_buy_position(df)
+        stock_df = pdr.get_data_yahoo(ticker)
+        stock = Stock(ticker, stock_df)
+        stock.get_indicator()
 
-        return simulation(ticker, df)
+        return stock.simulation()
     except Exception as e:
         print("except : " + ticker)
         print(e)
         pass
 
-
+# 주식 전체 조회
 def buyStock():
     print("------------주식-----------")
-    # #주식
-    stock = []
+    stocks = []
     for ticker in tqdm(stock_tickers["Symbol"][:100]):
         try:
-            df = pdr.get_data_yahoo(ticker)
-            df = get_buy_position(df)
-            if simulation(ticker, df) is not None:
-                stock.append(simulation(ticker, df))
-            time.sleep(0.01)
-            print(stock)
+            stock_df = pdr.get_data_yahoo(ticker)
+            stock = Stock(ticker, stock_df)
+            stock.get_indicator()
+            stock.simulation()
+
+            if stock.buy_score >= 3:
+                print(stock)
+                stocks.append(stock_to_string(stock))
+
         except Exception as e:
-            print("except : " + ticker)
+            print("except")
             print(e)
             pass
+    print(stocks)
+    return stocks
 
-    return stock
-
-
+# 주식 단일 종목 조회
 def buyOneStock(ticker):
     print("------------주식 하나만-----------")
 
     # 주식
     try:
-        df = pdr.get_data_yahoo(ticker)
-        df = get_buy_position(df)
-        return simulation(ticker, df)
+        stock_df = pdr.get_data_yahoo(ticker)
+        stock = Stock(ticker, stock_df)
+        stock.get_indicator()
+
+        return stock.simulation()
 
     except Exception as e:
         print("except : " + ticker)
