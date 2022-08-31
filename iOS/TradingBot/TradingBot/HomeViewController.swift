7f74c38b07d06483ed9a2d756c563945c6d834f0
sbcheon <suebin710@gmail.com>
2022-08-31 11:07:44 +0900

    remove: Python logic
    - PythonKit은 iOS가 아닌 것으로 파악되어 우선 관련 코드 모두 제거

--- iOS/TradingBot/TradingBot/HomeViewController.swift
+++ iOS/TradingBot/TradingBot/HomeViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
