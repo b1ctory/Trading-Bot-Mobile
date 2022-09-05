c56cc19e3727218deae482354506da426f32a3bb
sbcheon <suebin710@gmail.com>
2022-09-05 16:23:55 +0900

    update: TradingBot iOS SDK

--- TradingBotSDK/TradingBotSDK/TradingBot.swift
+++ TradingBotSDK/TradingBotSDK/TradingBot.swift
@@ -7,8 +7,37 @@
 
 import Foundation
 
-open class TradingBot {
+public class TradingBot {
+    
+    public var etfArr: [[String]] = []
+    
+    public init() {
+    }
+    
     public func setup() {
         print("TradingBot")
+    }
+    
+    public func loadCSV() {
+        let path = Bundle.main.path(forResource: "data/ETFs", ofType: "csv")!
+        parseCSVAt(url: URL(fileURLWithPath: path))
+        
+        print(etfArr)
+    }
+    
+    public func parseCSVAt(url: URL) {
+        do {
+            let data = try Data(contentsOf: url)
+            let dataEncoded = String(data: data, encoding: .utf8)
+            
+            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({ $0.components(separatedBy: ",") }) {
+                for item in dataArr {
+                    etfArr.append(item)
+                }
+            }
+            
+        } catch {
+            print("Error reading CSV Files")
+        }
     }
 }
