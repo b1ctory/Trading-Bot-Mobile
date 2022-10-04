7a31236659e49a084379cc1c65b58d401f9d1ddb
sbcheon <suebin710@gmail.com>
2022-10-04 17:23:36 +0900

    add: getCSV Data (stock_tickers)

--- iOS/TradingBot/TradingBot/HomeViewController.swift
+++ iOS/TradingBot/TradingBot/HomeViewController.swift
@@ -16,6 +16,25 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         print(TradingBotSDKVersionNumber)
         
+        print(getStockTickers())
+    }
+    
+    func getStockTickers() -> [[String]]? {
+        do {
+            let path = Bundle.main.path(forResource: "stock_tickers", ofType: "csv")!
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let dataEncoded = String(data: data, encoding: .utf8)
+            
+            let parsedCSV = dataEncoded?.components(separatedBy: "\n").map({
+                
+                $0.components(separatedBy: ",")
+            })
+             
+            return parsedCSV ?? nil
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
     }
     
 }
